da3bb8e9c3872c7b623120e8b94788f1905c4fce
merit <merit_abshire@davincischools.org>
2017-12-05 14:34:19 -0800

    Create sunset.js

---
+++ phasesix/sunset.js
@@ -0,0 +1,11 @@
+function showPrice(elem){
+	elem.classList.toggle("one");
+}
+
+function showImage(elem){
+	elem.classList.toggle("two");
+}
+
+function playCheer(e){
+	document.getElementById("boop").play();
+}
