c20a8f0c0d82a158b562db55f6d0a48fba95dd2b
merit <merit_abshire@davincischools.org>
2017-12-06 13:55:57 -0800

    Update sunset.js

--- phasesix/sunset.js
+++ phasesix/sunset.js
@@ -2,6 +2,8 @@ function showPrice(elem){
 	elem.classList.toggle("one");
 }
 
+// this function is relative to the images in my html that are priced at $1. My css file contains the image url. This also works the same as the "showImage" function; the only difference is they are showing different prices. 
+
 function showImage(elem){
 	elem.classList.toggle("two");
 }
@@ -9,3 +11,5 @@ function showImage(elem){
 function playSound(e){
 	document.getElementById("boop").play();
 }
+
+// this function is relative to the div class, "soon" because it triggers a sound file in the mainframe by grabbing its id and playing it once the div is clicked.
